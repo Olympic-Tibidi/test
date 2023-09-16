b2d7f2983bfc707b8a3e6b6fb7f5e2efb729352a
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-09-16 10:28:08 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -493,7 +493,7 @@ if authentication_status:
                     def color_coding(row):
                         return ['color:red'] * len(row) if row['CLEARWATER - LEWISTON ID'] == (5,5) else ['color:green'] * len(row)
                     #st.dataframe(df.style.apply(color_coding, axis=1))
-                    df=df.style.applymap(lambda x: f"color: {'red' if isinstance(x,str) else 'black'}")
+                    #df=df.style.applymap(lambda x: f"color: {'red' if isinstance(x,str) else 'black'}")
                     return df,zf
                 
 

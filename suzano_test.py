16cf7a048bb453add836aeaffd826c9e63745060
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-09-16 08:29:12 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -348,8 +348,8 @@ if authentication_status:
                     st.write("NO EDI FILES IN DIRECTORY")
                                                                                  
             with admin_tab5:
-                current_schedule=gcp_download(target_bucket,rf"mill_shipments.json")
-                schedule=pd.read_excel(uploaded_file,sheet_name="SEPTEMBER",header=None,index_col=None)
+                current_schedule=gcp_download(target_bucket,rf"truck_schedule.xlsx")
+                schedule=pd.read_excel(current_schedule,sheet_name="SEPTEMBER",header=None,index_col=None)
                 schedule=schedule.dropna(0, how="all")
                 schedule.reset_index(drop=True,inplace=True)
                 class Mill:

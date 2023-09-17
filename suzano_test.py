73eae4ff1f5da301c265ec6e7e1a73a9353eeece
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-09-16 18:54:10 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -522,7 +522,7 @@ if authentication_status:
                         for ix in truck_schedule.index:
                             total=(total[0]+truck_schedule.loc[ix,col][0],total[1]+truck_schedule.loc[ix,col][1])
                         totals.append(total)
-                    st.write(truck_schedule.columns)       
+                    
                         
                     truck_schedule.loc["TOTAL"]=totals
                     choice=st.radio("TRUCK LOADS OR TONS",["TRUCKS","TONS"])                   
@@ -541,6 +541,15 @@ if authentication_status:
                                 else:
                                     ton_schedule.at[ix,i]=(ton_schedule.loc[ix,i][0]*20,ton_schedule.loc[ix,i][1]*20)
                         ton_schedule["Total"]= ton_schedule.apply(lambda row: elementwise_sum(row['GP WAUNA - OR'], row['CLEARWATER - LEWISTON ID'],row['GP HALSEY - OR'],row['KROGER - BC'], row['WILLAMETTE FALLS - OR']),axis=1)
+                        totals=[]
+                        for col in ton_schedule.columns:  
+                            total=(0,0)
+                            for ix in ton_schedule.index:
+                                total=(total[0]+ton_schedule.loc[ix,col][0],total[1]+ton_schedule.loc[ix,col][1])
+                            totals.append(total)
+                    
+                        
+                        ton_schedule.loc["TOTAL"]=totals
                     
                         st.table(pd.DataFrame(ton_schedule))
                                 

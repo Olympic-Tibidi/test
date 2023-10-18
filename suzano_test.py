ad679b5fc0bed739db20f12ad012e15bd46c9479
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-10-18 08:46:48 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -343,6 +343,7 @@ if authentication_status:
                 storage_client = storage.Client()
                 bucket = storage_client.bucket(bucket_name=bucket_name)
                 blobs = bucket.list_blobs(prefix=prefix)  # Get list of files
+                st.write(blobs)
                 for blob in blobs:
                     filename = blob.name.replace('/', '_') 
                     blob.download_to_filename(dl_dir + filename)  # Download

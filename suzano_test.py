96921a3ec5ad61ac2460b96b0930b66cd80d3fb2
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-10-18 07:48:25 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -337,7 +337,7 @@ if authentication_status:
         if select=="DATA BACKUP" :
             if st.button("DD"):
                 
-                def download_bucket_with_transfer_manager(bucket_name, destination_directory="", workers=8, max_results=1000):
+                def download_bucket_with_transfer_manager(bucket_name, destination_directory=destination_directory, workers=8, max_results=1000):
                     """Download all of the blobs in a bucket, concurrently in a process pool.
                 
                     The filename of each blob once downloaded is derived from the blob name and

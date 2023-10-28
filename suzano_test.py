088c817c8a4dafd76fe49029d6ad4966c1bb6a40
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-10-28 10:18:00 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -1805,7 +1805,7 @@ if authentication_status:
                                         
                     arrived_vehicles=pd.DataFrame(arrived_vehicles)
                     arrived_vehicles=arrived_vehicles.rename_axis('TRUCK NO')    
-                    today_arrived_vehicles=pd.DataFrame(arrived_vehicles)
+                    today_arrived_vehicles=pd.DataFrame(today_arrived_vehicles)
                     today_arrived_vehicles=today_arrived_vehicles.rename_axis('TRUCK NO')
                     enroute_vehicles=pd.DataFrame(enroute_vehicles)
                     enroute_vehicles=enroute_vehicles.rename_axis('TRUCK NO')

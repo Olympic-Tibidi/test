6e301ed2ed9f0ed7a1bdb551f1ecf33d60b9b29e
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-11-05 10:59:06 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -503,7 +503,7 @@ if authentication_status:
                         wrap_edit=st.text_input("Grade",to_edit[vessel][release_order_number][sales_order_item_edit]["grade"],disabled=True)
                         batch_edit=st.text_input("Batch No",to_edit[vessel][release_order_number][sales_order_item_edit]["batch"],disabled=True)
                         dryness_edit=st.text_input("Dryness",to_edit[vessel][release_order_number][sales_order_item_edit]["dryness"],disabled=True)
-                        admt_edit=st.text_input("ADMT PER UNIT",to_edit[vessel][release_order_number][sales_order_item_edit]["admt"],disabled=True)
+                        admt_edit=st.text_input("ADMT PER UNIT",round(int(batch_mapping[ocean_bill_of_lading_edit]["dryness"])/90,6),disabled=True)
                         unitized_edit=st.selectbox("UNITIZED/DE-UNITIZED",["UNITIZED","DE-UNITIZED"],disabled=False)
                         quantity_edit=st.number_input("Quantity of Units", min_value=1, max_value=5000, value=1, step=1,  key=None, help=None, on_change=None, disabled=False, label_visibility="visible")
                         tonnage_edit=2*quantity

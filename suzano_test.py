5d4564e247d0db52edd9ff304ecf9352ab7bd6bd
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-11-22 09:46:01 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -434,7 +434,7 @@ if authentication_status:
                 # Display the updated DataFrame
                 st.write("# Updated Cost Table")
                 display=pd.DataFrame(st.session_state.scores)
-                display.loc["TOTAL FOR SHIFT"]=display["Quantity","Hours","OT","Hours Cost","OT Cost","Total Wage","Benefits&PMA","TOTAL COST","Markup","INVOICE"].sum()
+                #display.loc["TOTAL FOR SHIFT"]=display["Quantity","Hours","OT","Hours Cost","OT Cost","Total Wage","Benefits&PMA","TOTAL COST","Markup","INVOICE"].sum()
                 st.dataframe(display)
                 index=st.number_input("Enter Index To Delete",step=1,key="1224aa")
                 if st.button("DELETE BY INDEX"):

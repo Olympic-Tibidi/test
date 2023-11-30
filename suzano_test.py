48c14f9fb5766b75d14465fd46ebfc3b3bd05622
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-11-30 14:36:13 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -555,7 +555,14 @@ if authentication_status:
                         # Create a temporary local file to store the downloaded MP3 file
                         temp_file_path = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3").name
                         blob.download_to_filename(temp_file_path)
-
+                        st.write(
+                                """
+                                <script>
+                                    const audio = new Audio("{temp_file_path}");
+                                    audio.play();
+                                </script>
+                                """
+    )
                         audio_file = open(temp_file_path, "rb").read()
                         st.audio(audio_file, format="audio/mp3")
                         led_col1,led_col2,led_col3,led_col4=st.columns([3,2,2,2])

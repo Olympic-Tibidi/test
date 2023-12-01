67df120eb24bbc4b02452be064db99122f30ee03
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-01 09:02:06 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2085,6 +2085,7 @@ if authentication_status:
                             
                             for i in release_order_dictionary:
                                 for sales in release_order_dictionary[i]:
+                                    st.write(i,sales)
                                     release_order_dest_map[i]=release_order_dictionary[i][sales]["destination"]
                             
                             destinations_of_release_orders=[f"{i} to {release_order_dest_map[i]}" for i in files_in_folder]

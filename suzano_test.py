3f1c39db9dc7f4cc970422d1e03e2417ee143254
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-01 14:33:25 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2324,7 +2324,7 @@ if authentication_status:
                                     pass
                             st.markdown("**CURRENT DISPATCH QUEUE**")
                             try:
-                                dispatch=gcp_download("olym_suzano",rf"dispatched.json")
+                                dispatch=gcp_download(target_bucket,rf"dispatched.json")
                                 dispatch=json.loads(dispatch)
                                 try:
                                     for dispatched_release in dispatch.keys():

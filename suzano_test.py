c180f3c0adacd6f8e1a6136c4a2fe7a784e6e9a4
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-02 17:48:29 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2232,13 +2232,14 @@ if authentication_status:
                         for i in release_order_dictionary[vessel]:
                             if i in completed_release_orders:
                                 completed_release_order_dest_map[i]=release_order_dictionary[vessel][i][sales]#["destination"]
-                        st.write(pd.DataFrame(completed_release_order_dest_map).T)
-                        destinations_of_completed_release_orders=[f"{i} to {completed_release_order_dest_map[i]}" for i in completed_release_orders]
+                        if len(pd.DataFrame(completed_release_order_dest_map).T)!>1:
+                            st.write(pd.DataFrame(completed_release_order_dest_map).T)
+                            destinations_of_completed_release_orders=[f"{i} to {completed_release_order_dest_map[i]}" for i in completed_release_orders]
                     
                                                                     
-                        requested_file_=st.selectbox("COMPLETED RELEASE ORDERS",destinations_of_completed_release_orders,key=16)
-                        requested_file=requested_file_.split(" ")[0]
-                        nofile=0
+                            requested_file_=st.selectbox("COMPLETED RELEASE ORDERS",destinations_of_completed_release_orders,key=16)
+                            requested_file=requested_file_.split(" ")[0]
+                            nofile=0
                     with rls_tab3:
                         mf_numbers_=gcp_download(target_bucket,rf"release_orders/mf_numbers.json")
                         mf_numbers=json.loads(mf_numbers_)

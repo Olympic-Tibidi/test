343d5c829c1f831b3d6496d66f7b9bc6c0127676
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-02 17:44:25 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2242,9 +2242,9 @@ if authentication_status:
                     with rls_tab3:
                         mf_numbers_=gcp_download(target_bucket,rf"release_orders/mf_numbers.json")
                         mf_numbers=json.loads(mf_numbers_)
-                        gp_release_orders=[i for i in release_order_database if release_order_database[i]["001"]["destination"] in ["GP-Clatskanie,OR","GP-Halsey,OR"] and release_order_database[i]["001"]["remaining"]>2]
-                        vessel_mf=st.selectbox("SELECT VESSEL",["KIRKENES-2304"],key="lalala")
-                        destinations_of_release_orders=[f"{i} to {release_order_dest_map[i]}" for i in release_order_database if release_order_database[i]["001"]["destination"] in ["GP-Clatskanie,OR","GP-Halsey,OR"] and release_order_database[i]["001"]["remaining"]>2]
+                        vessel_mf=st.selectbox("SELECT VESSEL",["KIRKENES-2304","JUVENTAS-2308"],key="lalalda")
+                        gp_release_orders=[i for i in release_order_database[vessel_mf] if release_order_database[vessel_mf][i]["001"]["destination"] in ["GP-Clatskanie,OR","GP-Halsey,OR"] and release_order_database[vessel_mf][i]["001"]["remaining"]>2]
+                        destinations_of_release_orders=[f"{i} to {release_order_dest_map[i]}" for i in release_order_database[vessel] if release_order_database[vessel][i]["001"]["destination"] in ["GP-Clatskanie,OR","GP-Halsey,OR"] and release_order_database[vessel][i]["001"]["remaining"]>2]
                         release_order_number_mf=st.selectbox("ACTIVE RELEASE ORDERS",destinations_of_release_orders,key="tatata")
                         input_mf_numbers=st.text_area("**ENTER MF NUMBERS**",height=100,key="juy")
                         if input_mf_numbers is not None:

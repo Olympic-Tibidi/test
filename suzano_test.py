b26bd04ab0b6fdd882eae4f4f3b3d1bf22ec19d0
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-05 09:38:05 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2448,15 +2448,19 @@ if authentication_status:
                         mf=True
                         load_mf_number_issued=False
                         if carrier_code=="123456-KBX":
-                           if release_order_number in mf_numbers_for_load[vessel].keys():
-                               mf_liste=[i for i in mf_numbers_for_load[vessel][release_order_number]]
-                               load_mf_number=st.selectbox("MF NUMBER",mf_liste,disabled=False,key=14551)
-                               mf=True
-                               load_mf_number_issued=True
+                           if vessel in mf_numbers_for_load:
+                               
+                               if release_order_number in mf_numbers_for_load[vessel].keys():
+                                   mf_liste=[i for i in mf_numbers_for_load[vessel][release_order_number]]
+                                   load_mf_number=st.selectbox("MF NUMBER",mf_liste,disabled=False,key=14551)
+                                   mf=True
+                                   load_mf_number_issued=True
+                               else:
+                                   st.write("MF NUMBERS NOT IN!")
+                                   mf=False
+                                   load_mf_number_issued=False  
                            else:
                                st.write("MF NUMBERS NOT IN!")
-                               mf=False
-                               load_mf_number_issued=False  
                         foreman_quantity=st.number_input("**:blue[ENTER Quantity of Units]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=8)
                         foreman_bale_quantity=st.number_input("**:blue[ENTER Quantity of Bales]**", min_value=0, max_value=30, value=0, step=1, help=None, on_change=None, disabled=False, label_visibility="visible",key=123)
                     click_clear1 = st.button('CLEAR VEHICLE-QUANTITY INPUTS', key=34)

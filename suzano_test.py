b09b6027340471fb9f7bd4fd9b72af19efe2ebe4
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-07 15:15:53 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2746,7 +2746,15 @@ if authentication_status:
                                         with st.expander(f"VERIFY THIS UNIT CAME FROM {vessel} - {'Unwrapped' if grade=='ISU' else 'wrapped'} piles"):
                                             st.write("The chart above shows some numbers I picked for you.I rolled actual dice for these, so they're *guaranteed* tobe random.")
                                             if st.button("ADD UNIT TO INVENTORY"):
-                                                st.write("Do something")
+                                                {"KIRKENES-2304": {"I1132857": {"Batch": "45302855", "Ocean_bl": "GSSWKIR6013D"}, 
+                                                updated_bill=bill_mapping
+                                                updated_bill[vessel][x[:-2]]={"Batch":batch,"Ocean_bl":ocean_bill_of_lading}
+                                                updated_bill=json.dumps(updated_bill)
+                                                storage_client = storage.Client()
+                                                bucket = storage_client.bucket(target_bucket)
+                                                blob = bucket.blob(rf"bill_mapping.json")
+                                                blob.upload_from_string()
+                                                st.success(f"Added Unit {x} to Inventory!",icon="✅")
                                         seen.add(x)
                                 
                         if bale_load_input is not None:

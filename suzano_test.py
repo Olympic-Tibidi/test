ebe141e72495c95abead4726e1bd073c8aed3da8
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-09 19:13:21 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2081,15 +2081,11 @@ if authentication_status:
                         if edit:
                             release_order_database[vessel][release_order_number][sales_order_item_edit]={"destination":destination_edit,"total":quantity_edit,"remaining":remaining_edit}
                         else:
+                            release_order_database[vessel][release_order_number]={}
+                            release_order_database[vessel][release_order_number][sales_order_item]={}
                             release_order_database[vessel][release_order_number][sales_order_item]={"destination":destination,"total":quantity,"remaining":quantity}
-                                
-                                                         
-                                #release_order_database[vessel]={}
-                                #release_order_database[vessel][release_order_number]={}
-                                #release_order_database[vessel][release_order_number][sales_order_item]={}
-                                #release_order_database[vessel][release_order_number][sales_order_item]={"destination":destination,"total":quantity,"remaining":quantity}
-                                
                             st.write(f"Recorded Release Order - {release_order_number} for Item No: {sales_order_item}")
+                        
                         release_orders_json=json.dumps(release_order_database)
                         storage_client = storage.Client()
                         bucket = storage_client.bucket(target_bucket)

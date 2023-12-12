c6bd084acebe53b8564ea161a4939c37e66038e0
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-12 07:51:32 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2091,6 +2091,7 @@ if authentication_status:
                                                   'shipped':qt,'remaining':ro.loc[int(val),"KIRKENES-2304"]['001']['remaining']}})
                     new=pd.DataFrame(info).T
                     new=new.reset_index()
+                    new.columns=["Release Order","Ocean_BOL","Destination","Total Order", "Shipped", "Remaining"]
                     new.groupby('level_1')['remaining'].sum()
                     
                     release_orders = [str(key[0]) for key in info.keys()]

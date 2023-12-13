8106de96754e440f0bd7b880a857395b28bd99cf
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-13 06:35:21 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2161,7 +2161,7 @@ if authentication_status:
                         po_number_edit=st.text_input("PO No",to_edit[release_order_number]["po_number"],disabled=False)
                         destination_edit=st.text_input("Destination",to_edit[release_order_number]["destination"],disabled=False)
                         sales_order_item_edit=st.selectbox("Sales Order Item",list(to_edit[release_order_number].keys())[2:],disabled=False)
-                        vessel_edit=st.text_input("Vessel",to_edit[release_order_number][sales_order_item_edit]['vessel'],disabled=False)
+                        vessel_add=vessel=st.selectbox("SELECT VESSEL",["KIRKENES-2304","JUVENTAS-2308"],key="poFpoa")
                         ocean_bill_of_lading_edit=st.text_input("Ocean Bill Of Lading",to_edit[release_order_number][sales_order_item_edit]["ocean_bill_of_lading"],disabled=False)
                         wrap_edit=st.text_input("Grade",to_edit[release_order_number][sales_order_item_edit]["grade"],disabled=False)
                         batch_edit=st.text_input("Batch No",to_edit[release_order_number][sales_order_item_edit]["batch"],disabled=False)

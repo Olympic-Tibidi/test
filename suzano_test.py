ac4ab3ed2eac34cd5fd59a9c9a9a76bda221f08f
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-28 08:47:17 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2932,7 +2932,6 @@ if authentication_status:
                                 st.session_state.load_mf_number = None
                             if release_order_number in mf_numbers_for_load.keys():
                                 mf_liste=[i for i in mf_numbers_for_load[release_order_number]]
-                                st.write(mf_liste)
                                 if len(mf_liste)>0:
                                     try:
                                         load_mf_number = st.selectbox("MF NUMBER", mf_liste, disabled=False, key=14551, index=mf_liste.index(st.session_state.load_mf_number) if st.session_state.load_mf_number else 0)
@@ -4110,7 +4109,10 @@ if authentication_status:
                         if release_order_number in mf_numbers_for_load.keys():
                             mf_liste=[i for i in mf_numbers_for_load[release_order_number]]
                             if len(mf_liste)>0:
-                                load_mf_number = st.selectbox("MF NUMBER", mf_liste, disabled=False, key=14551, index=mf_liste.index(st.session_state.load_mf_number) if st.session_state.load_mf_number else 0)
+                                try:
+                                    load_mf_number = st.selectbox("MF NUMBER", mf_liste, disabled=False, key=14551, index=mf_liste.index(st.session_state.load_mf_number) if st.session_state.load_mf_number else 0)
+                                except:
+                                    load_mf_number = st.selectbox("MF NUMBER", mf_liste, disabled=False, key=14551)
                                 mf=True
                                 load_mf_number_issued=True
                                 yes=True

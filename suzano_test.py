58f8b27e07aefa168af0c7c8a7820ea85bcc7e0c
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-28 06:24:57 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -2150,6 +2150,7 @@ if authentication_status:
                             st.dataframe(new)
                         with relcol2:
                             st.plotly_chart(fig)
+                        
                         temp_dict={}
                         for rel_ord in raw_ro:
                             
@@ -2162,14 +2163,21 @@ if authentication_status:
                                 remaining=raw_ro[rel_ord][sales]['remaining']
                                 temp_dict[rel_ord,sales]={'destination': dest,'vessel': vessel,'total':total,'remaining':remaining}
                         temp_df=pd.DataFrame(temp_dict).T
+                      
+                        temp_df= temp_df.rename_axis(['release_order','sales_order'], axis=0)
+                    
                         temp_df['First Shipment'] = temp_df.index.map(inv_bill_of_ladings.groupby(['release_order','sales_order'])['issued'].first())
                         
                         for i in temp_df.index:
                             if temp_df.loc[i,'remaining']<=2:
-                                temp_df.loc[i,"Last Shipment"]=inv_bill_of_ladings.groupby(['release_order','sales_order']).issued.last().loc[i]
+                                try:
+                                    temp_df.loc[i,"Last Shipment"]=inv_bill_of_ladings.groupby(['release_order','sales_order']).issued.last().loc[i]
+                                except:
+                                    temp_df.loc[i,"Last Shipment"]=datetime.datetime.now()
                                 temp_df.loc[i,"Duration"]=(pd.to_datetime(temp_df.loc[i,"Last Shipment"])-pd.to_datetime(temp_df.loc[i,"First Shipment"])).days+1
                         
-                        temp_df['Last Shipment'] = temp_df['Last Shipment'].fillna(datetime.datetime.now())
+                        temp_df['First Shipment'] = temp_df['First Shipment'].fillna(datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d %H:%M:%S'))
+                        temp_df['Last Shipment'] = temp_df['Last Shipment'].fillna(datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d %H:%M:%S'))
                         
                         ####
                         
@@ -2179,7 +2187,11 @@ if authentication_status:
                         df_temp=inv_bill_of_ladings.copy()
                         df_temp["issued"]=[pd.to_datetime(i).date() for i in df_temp["issued"]]
                         for i in temp_df.index:
-                            temp_df.loc[i,"Utilized Shipment Days"]=df_temp.groupby(["release_order",'sales_order'])[["issued"]].nunique().loc[i,'issued']
+                            try:
+                                temp_df.loc[i,"Utilized Shipment Days"]=df_temp.groupby(["release_order",'sales_order'])[["issued"]].nunique().loc[i,'issued']
+                            except:
+                                temp_df.loc[i,"Utilized Shipment Days"]=0
+                        
                         temp_df['First Shipment'] = temp_df['First Shipment'].apply(lambda x: datetime.datetime.strftime(datetime.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'),'%d-%b,%Y'))
                         temp_df['Last Shipment'] = temp_df['Last Shipment'].apply(lambda x: datetime.datetime.strftime(datetime.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'),'%d-%b,%Y') if type(x)==str else None)
                         liste=['Duration','# of Shipment Days',"Utilized Shipment Days"]

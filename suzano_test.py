dde6d552dc0a1a94d3a354e732e621fe3dd33f22
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2023-12-31 15:52:26 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -822,10 +822,14 @@ if authentication_status:
                             if job_no not in mt_jobs[year]:
                                 mt_jobs[year][job_no]={}
                             if "RECORDS" not in mt_jobs[year][job_no]:
-                                mt_jobs[year][job_no]["RECORDS"]={"LABOR":{"DOCK":{},"WAREHOUSE":{}},"EQUIPMENT":{"DOCK":{},"WAREHOUSE":{}},"MAINTENANCE":{"DOCK":{},"WAREHOUSE":{}}}
-                            mt_jobs[year][job_no]["RECORDS"]["LABOR"][work_type]=st.session_state.scores.T.to_dict()
-                            mt_jobs[year][job_no]["RECORDS"]["EQUIPMENT"][work_type]=st.session_state.eq_scores.T.to_dict()
-                            mt_jobs[year][job_no]["RECORDS"]["MAINTENANCE"][work_type]=st.session_state.maint_scores.T.to_dict()
+                                mt_jobs[year][job_no]["RECORDS"]={}
+                            if str(work_date) not in mt_jobs[year][job_no]["RECORDS"]:
+                                mt_jobs[year][job_no]["RECORDS"][str(work_date)]={}
+                            if st.session_state.shift not in mt_jobs[year][job_no]["RECORDS"][str(work_date)]:
+                                mt_jobs[year][job_no]["RECORDS"][str(work_date)][st.session_state.shift]={"LABOR":{"DOCK":{},"WAREHOUSE":{}},"EQUIPMENT":{"DOCK":{},"WAREHOUSE":{}},"MAINTENANCE":{"DOCK":{},"WAREHOUSE":{}}}
+                            mt_jobs[year][job_no]["RECORDS"][str(work_date)][st.session_state.shift]['LABOR'][work_type]=st.session_state.scores.T.to_dict()
+                            mt_jobs[year][job_no]["RECORDS"][str(work_date)][st.session_state.shift]['EQUIPMENT'][work_type]=st.session_state.eq_scores.T.to_dict()
+                            mt_jobs[year][job_no]["RECORDS"][str(work_date)][st.session_state.shift]['MAINTENANCE'][work_type]=st.session_state.maint_scores.T.to_dict()
                             st.write(mt_jobs)
                             mt_jobs_=json.dumps(mt_jobs)
                             storage_client = storage.Client()

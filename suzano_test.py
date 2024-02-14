c89b4b842e1c0eaceda3ff7796da9ef31fa395b6
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-02-14 09:43:21 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -51,7 +51,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 #from IPython.display import IFrame
 from reportlab.platypus import Table, TableStyle
-import fitz
+import pypdfium2
 #import streamlit_option_menu
 #from streamlit_modal import Modal
 

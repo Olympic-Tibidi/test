d0e618b70283898e8b824e7783ac53ce91a8fca9
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-02-14 09:01:15 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -3781,6 +3781,7 @@ if authentication_status:
                 pdf_bytes = create_pdf()
                 base64_pdf = base64.b64encode(pdf_bytes).decode('utf-8')
                 pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+                pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
                 st.markdown(pdf_display, unsafe_allow_html=True)
 
 

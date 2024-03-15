6ecf8bf5e23d17819d449d9e03d3c38ab4dc7425
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-15 12:53:53 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -1199,9 +1199,22 @@ with Profiler():
                             goahead=True
                             if goahead:
                                 
-                                gp_release_orders=[i for i in release_order_database if release_order_database[i]["001"]["destination"] in ["GP-Clatskanie,OR","GP-Halsey,OR"] ]   # and (release_order_database[i]["001"]["remaining"]>0|release_order_database[i]["001"]["remaining"]>0)
+                                def check_home(ro):
+                                    keys=[sale for sale in release_order_database[ro]]
+                                    destination=release_order_database[ro][keys[0]]['destination']
+                                    remains=[release_order_database[ro][key]["remaining"] for key in keys]
+                                    #print(sum(remains))
+                                    result=True
+                                    if destination not in ["GP-Clatskanie,OR","GP-Halsey,OR"]:
+                                        result=False 
+                                    if sum(remains)==0:
+                                        result=False
+                                    
+                                    return result
                                 
-                                destinations_of_release_orders=[f"{i} to {release_order_dest_map[i]}" for i in release_order_database if release_order_database[i]["001"]["destination"] in ["GP-Clatskanie,OR","GP-Halsey,OR"]]
+                                #gp_release_orders=[i for i in release_order_database if release_order_database[i]["001"]["destination"] in ["GP-Clatskanie,OR","GP-Halsey,OR"] ]   # and (release_order_database[i]["001"]["remaining"]>0|release_order_database[i]["001"]["remaining"]>0)
+                                
+                                destinations_of_release_orders=[f"{i} to {release_order_dest_map[i]}" for i in release_order_database if check_home(i)]
                                 if len(destinations_of_release_orders)==0:
                                     st.warning("NO GP RELEASE ORDERS FOR THIS VESSEL")
                                 else:

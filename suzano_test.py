d8a8451a1ede282fa2b3fe0eedc495d539fdc60c
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-16 18:56:08 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -743,6 +743,7 @@ with Profiler():
                                         else:
                                             st.markdown(f"**:blue[Sales Order Item] : {targets[1]}**")
                                         st.markdown(f"**:blue[Destination : {target['destination']}]**")
+                                        st.write(target[targets[1]])
                                         st.write(f"        Total Quantity : {target[targets[1]]['quantity']} Units - {2*target[targets[1]]['total']} Metric Tons")                        
                                         st.write(f"        Ocean Bill Of Lading : {target[targets[1]]['ocean_bill_of_lading']}")
                                         st.write(f"        Batch : {target[targets[1]]['batch']} WIRES : {target[targets[1]]['unitized']}")

00d8798268e79baa9d4988c187182960d5d62f5c
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-17 09:22:17 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -524,13 +524,14 @@ with Profiler():
                 with admin_tab3:
                     edi_files=list_files_in_subfolder(target_bucket, rf"EDIS/")
                     requested_edi_file=st.selectbox("SELECT EDI",edi_files[1:])
+                    data=gcp_download(target_bucket, rf"EDIS/{requested_edi_file}"),
                     try:
-                        #requested_edi=gcp_download(target_bucket, rf"EDIS/{requested_edi_file}")
+                       
                         st.text_area("EDI",requested_edi,height=400)                                
                        
                         st.download_button(
                             label="DOWNLOAD EDI",
-                            data=gcp_download(target_bucket, rf"EDIS/{requested_edi_file}"),
+                            data=data,
                             file_name=f'{requested_edi_file}',
                             mime='text/csv')
     

0ab3ee8d3bcc1f1066a0bb05680685995b555af0
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-17 12:26:30 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -64,7 +64,7 @@ st.set_page_config(layout="wide")
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "client_secrets.json"
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = st.secrets['private_key']
 
-target_bucket="new_suzano"
+target_bucket="new_suzano_spare"
 utc_difference=7
 
 def check_password():

d86d422871f037ed157f055b9972ca4a9671b3c3
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-23 15:11:32 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -738,10 +738,10 @@ if authentication_status:
 
 
                     with rls_tab4:  #####  SCHEDULE
-                        bill_data=gcp_download(target_bucket,rf"terminal_bill_of_ladings.json")
-                        admin_bill_of_ladings=json.loads(bill_data)
-                        admin_bill_of_ladings=pd.DataFrame.from_dict(admin_bill_of_ladings).T[1:]
-                        admin_bill_of_ladings["St_Date"]=[datetime.datetime.strptime(i,"%Y-%m-%d %H:%M:%S").date() for i in admin_bill_of_ladings["issued"]]
+                        # bill_data=gcp_download(target_bucket,rf"terminal_bill_of_ladings.json")
+                        # admin_bill_of_ladings=json.loads(bill_data)
+                        # admin_bill_of_ladings=pd.DataFrame.from_dict(admin_bill_of_ladings).T[1:]
+                        # admin_bill_of_ladings["St_Date"]=[datetime.datetime.strptime(i,"%Y-%m-%d %H:%M:%S").date() for i in admin_bill_of_ladings["issued"]]
                         display_df=admin_bill_of_ladings[admin_bill_of_ladings["St_Date"]==now.date()]
                         st.write(display_df)
                         

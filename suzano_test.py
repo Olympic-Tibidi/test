e8a4b912eb470b7a39e0f3404ee22e8fedb8ad46
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-03-23 19:57:03 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -749,6 +749,7 @@ if authentication_status:
                         schedule_frame=pd.DataFrame(schedule).T
                         #schedule_frame=schedule_frame.iloc[:-1]
                         for i in liste:
+                            st.write(i)
                             try:
                                 schedule_frame.loc[(schedule_frame['Release Order']==i[0])&(schedule_frame['Sales Order']==i[1]),"Loaded"]=0
                                 schedule_frame.loc[(schedule_frame['Release Order']==i[0])&(schedule_frame['Sales Order']==i[1]),"Loaded"]+=1

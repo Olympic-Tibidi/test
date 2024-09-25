3fb4c44f2cb1a510771c61fdaed5cc8c27f573cf
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-09-25 12:57:34 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -3231,10 +3231,10 @@ if authentication_status:
                                 if release_order_number_mf not in mf_numbers.keys():   
                                     mf_numbers[release_order_number_mf]={}
                                 if carrier_mf not in mf_numbers[release_order_number_mf]:
-                                    mf_numbers[release_order_number_mf]={}
+                                    #mf_numbers[release_order_number_mf]={}
                                     mf_numbers[release_order_number_mf][carrier_mf]=[]
                                 mf_numbers[release_order_number_mf][carrier_mf]+=input_mf_numbers
-                                mf_numbers[release_order_number_mf][carrier_mf]=list(set(mf_numbers[release_order_number_mf]))
+                                mf_numbers[release_order_number_mf][carrier_mf]=list(set(mf_numbers[release_order_number_mf][carrier_mf]))
                                 mf_data=json.dumps(mf_numbers)
                                 #storage_client = storage.Client()
                                 storage_client = get_storage_client()

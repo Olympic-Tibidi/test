842b22029924ba8b1909a8d42d0a60eef9734077
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-10-14 14:09:02 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -603,6 +603,7 @@ if authentication_status:
                     budget1=json.loads(gcp_download(target_bucket,rf"FIN/NEW/budget1.json"))
                     budget_2023=json.loads(gcp_download(target_bucket,rf"FIN/NEW/budget_2023.json"))
                     budget_2024=json.loads(gcp_download(target_bucket,rf"FIN/NEW/budget_2024.json"))
+                    ledger_b = pd.read_feather(io.BytesIO(ledger_b_content))
                     ledger_b = ledger_b.set_index("index", drop=True).reset_index(drop=True)
                     
                     ledger_b=ledger_b[ledger_b["Date"]<pd.Timestamp(datetime.date(2024,upto_month,1))]

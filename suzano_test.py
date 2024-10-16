5e50a33f28a1951c844cf448cb5216f6d6d628e6
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-10-15 19:38:06 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -1247,7 +1247,7 @@ if authentication_status:
                             a.loc["Revenue - Other"]=other_income.iloc[-1,:-1]
                             a.loc["Expense - Running Cost"]=non_vessel_expense.iloc[-1,:-1]
                             a.loc["Expense - Overhead"]=overhead.iloc[-1,:-1]
-                            a.loc["Expense - Depreciation"]=depreciation.iloc[-1,:-1]
+                            a.loc["Expense - Depreciation"]=-depreciation.iloc[-1,:-1]
                         else:
                             
                             a=pd.DataFrame(columns=range(1,upto_month))

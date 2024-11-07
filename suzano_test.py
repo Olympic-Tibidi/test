75ab1b2d8263d49f4fd95963bde2edc46292b182
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-11-07 07:52:14 -0800

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -3514,7 +3514,7 @@ if authentication_status:
                             #scheduled.set_index('Destination',drop=True,inplace=True)
 
                             def style_row(row):
-                                location = scheduled['Destination']
+                                location = row['Destination']
                                 #shipment_status = row["Status"]
                                 
                                 # Define colors for different locations

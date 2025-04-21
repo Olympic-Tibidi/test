150445faef254d0f12903e2ba2814e057f14aa00
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2025-04-20 22:29:56 -0700

    Update suzano_test.py

--- suzano_test.py
+++ suzano_test.py
@@ -1375,8 +1375,8 @@ if authentication_status:
                         ### LOAD LEDGERS by year
                         main_json=gcp_download(target_bucket,rf"main.json")
                         main_json=json.loads(main_json)
-                        st.write(main_json)
-                        main = pd.DataFrame.from_dict(main_json, orient="index").reset_index(drop=True)
+                        
+                        main = pd.DataFrame(main_json).T
                         ledgers=main[main["Period_Year"]==int(year[-2:])]
                         ledgers["Account"]=ledgers["Account"].astype("str")
                         ledgers.set_index("index",drop=True,inplace=True)
